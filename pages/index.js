d3eb805922275b67ac7212dc9870c6137e2629c2
Erik Hebisch <flegelleicht@gmail.com>
2018-10-10 13:15:59 +0200

    Corrected typo on index page

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default () => (
     <h1>My Blog</h1>
     <ul>
       <PostLink title="Hello Next.js" />
-      <PostLink title="Learn Next.jsis awesome" />
+      <PostLink title="Learn Next.js is awesome" />
       <PostLink title="Deploy apps with Zeit" />
     </ul>
   </Layout>
