77c89cc21c3315c827891cb9e00c62b3040f935d
Erik Hebisch <flegelleicht@gmail.com>
2018-10-10 14:15:25 +0200

    Moved link specific style to respective component

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,22 @@ const PostLink = ({ post }) => (
     <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
       <a>{post.title}</a>
     </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: "Arial";
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </li>
 )
 
@@ -25,26 +41,8 @@ export default () => (
       ))}
     </ul>
     <style jsx>{`
-      h1, a { 
+      h1 { 
         font-family: sans-serif;
-      }
-      
-      ul {
-        padding: 0;
-      }
-      
-      li {
-        list-style: none;
-        margin: 5px 0;
-      }
-      
-      a {
-        text-decoration: none;
-        color: blue;
-      }
-      
-      a:hover {
-        opacity: 0.6
       }
     `}</style>
   </Layout>
